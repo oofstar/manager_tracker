2fa4dc2e8e3c523b57a74243d339df011f49c76f
Sebastian Kopp <smkopp92@gmail.com>
2017-09-06 17:08:43 -0400

    Simple pagination

--- react/src/components/KyleeList.js
+++ react/src/components/KyleeList.js
@@ -1,13 +1,39 @@
 import React from 'react';
 
-const KyleeList = (props) => {
-  let kylees = props.kylees.map((kylee) => <li key={kylee.id}>{kylee.name}</li>)
+class KyleeList extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      currentPage: 0
+    }
 
-  return(
-    <ul>
-      {kylees}
-    </ul>
-  )
+    this.nextPage = this.nextPage.bind(this)
+    this.prevPage = this.prevPage.bind(this)
+  }
+
+  nextPage(){
+    this.setState({currentPage: this.state.currentPage + 1})
+  }
+
+  prevPage(){
+    this.setState({currentPage: this.state.currentPage - 1})
+  }
+
+  render(){
+    let filteredKylees = this.props.kylees.slice(this.state.currentPage*10,(this.state.currentPage+1)*10)
+
+    let kylees = filteredKylees.map((kylee) => <li key={kylee.id}>{kylee.name}</li>)
+
+    return(
+      <div>
+        <ul>
+          {kylees}
+        </ul>
+        {this.state.currentPage == 0 ? null : <button onClick={this.prevPage}>Previous</button>}
+        {this.state.currentPage == Math.round(this.props.kylees.length/10) ? null : <button onClick={this.nextPage}>Next</button>}
+      </div>
+    )
+  }
 }
 
 export default KyleeList;
