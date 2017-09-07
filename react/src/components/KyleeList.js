1605841a1d66d8dc46681fa482da018b72d7f5b8
Sebastian Kopp <smkopp92@gmail.com>
2017-09-07 09:49:13 -0400

    Second Version of pagination with page numbers

--- react/src/components/KyleeList.js
+++ react/src/components/KyleeList.js
@@ -1,13 +1,65 @@
 import React from 'react';
 
-const KyleeList = (props) => {
-  let kylees = props.kylees.map((kylee) => <li key={kylee.id}>{kylee.name}</li>)
+class KyleeList extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      currentPage: 1,
+      kyleesPerPage: 5
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-  return(
-    <ul>
-      {kylees}
-    </ul>
-  )
+  handleClick(event) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+  }
+
+  render(){
+    let indexOfLastKylee = this.state.currentPage * this.state.kyleesPerPage;
+    let indexOfFirstKylee = indexOfLastKylee - this.state.kyleesPerPage;
+    let currentKylees = this.props.kylees.slice(indexOfFirstKylee, indexOfLastKylee);
+
+    let kylees = currentKylees.map((kylee) => <li key={kylee.id}>{kylee.name}</li>)
+
+    let pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(this.props.kylees.length / this.state.kyleesPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    let currentPageNumbers = pageNumbers.filter((page) => {
+      if(this.state.currentPage == 1 || this.state.currentPage == 2){
+        return (page >= 1 && page <= 5)
+      }else{
+        return (page >= this.state.currentPage - 2 && page <= this.state.currentPage + 2)
+      }
+    })
+
+    let renderPageNumbers = currentPageNumbers.map(number => {
+      return(
+        <li
+          key={`page ${number}`}
+          id={number}
+          onClick={this.handleClick}
+          className="block"
+        >
+          {number}
+        </li>
+      );
+    });
+
+    return(
+      <div>
+        <ul>
+          {kylees}
+        </ul>
+        <ul>
+          {renderPageNumbers}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default KyleeList;
